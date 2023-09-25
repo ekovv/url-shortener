7802a7d5466ab47c4fe224ccdeec1e7c6a1d6b59
ekovv <denisovdima2004@yandex.ru>
2023-09-25 22:00:00 +0300

    :tada:iter6

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 	"log"
 	"url-shortener/config"
 	"url-shortener/internal/app/handler"
+	myLog "url-shortener/internal/app/logger"
 	"url-shortener/internal/app/service"
 	"url-shortener/internal/app/storage"
 )
@@ -15,10 +17,17 @@ func main() {
 	sr := service.NewService(st, *conf)
 	h := handler.NewHandler(&sr)
 	router := gin.Default()
+	router.Use(myLog.HTTPLogger())
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	myLog.Sugar = *logger.Sugar()
 	router.POST("/", h.UpdateAndGetShort)
 	router.GET("/:id", h.GetLongURL)
 
-	err := router.Run(conf.Host)
+	err = router.Run(conf.Host)
 	if err != nil {
 		log.Fatalf("can't run http server: %v", err)
 		return
