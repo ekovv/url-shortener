aa1be33eb557b709dab75e9ed382d9039a51770b
ekovv <denisovdima2004@yandex.ru>
2023-10-04 18:15:25 +0300

    :tada:iter13

--- internal/storage/db_storage.go
+++ internal/storage/db_storage.go
@@ -8,7 +8,6 @@ import (
 	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/lib/pq"
-	_ "github.com/lib/pq"
 	"url-shortener/config"
 )
 
